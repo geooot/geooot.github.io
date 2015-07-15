bf6d137bff0a4a9d643def67d1f6ad1cd298e3d9
George Thayamkery <george.thayamkery@gmail.com>
2015-07-15 14:17:47 -0500

    New Project Page Template
    fdsfds

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $("document").ready(function(){
-	$("#btn1").hover(function(){
-		$("#g").animate(function(){
-			$(this).css('-webkit-transform','rotate('+90+'deg)'); 
-			$(this).css('-moz-transform','rotate('+90+'deg)');
-			$(this).css('transform','rotate('+90+'deg)');
-		},"linear")
-	})
+	$("#btn1").hover(function() {
+		$('#g').animate({  borderSpacing: -22.5 }, {
+			step: function(now,fx) {
+				$(this).css('-webkit-transform','rotate('+now+'deg)'); 
+				$(this).css('-moz-transform','rotate('+now+'deg)');
+				$(this).css('transform','rotate('+now+'deg)');
+			},
+			duration:'fast'
+		},'linear');
+	}
+
 })
